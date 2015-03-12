0488beee0abf945785fda2807be12e362edd025f
tfast <till@eduroam-056-041.scc.uni-weimar.de>
2015-03-12 12:59:05 +0100

    added framework files from fensterchen

---
+++ programmiersprachen-raytracer-master/framework/Ray.hpp
@@ -0,0 +1,17 @@
+//
+//  Ray.hpp
+//  fensterchen
+//
+//  Created by Till Fastnacht on 10/01/15.
+//
+//
+
+#ifndef fensterchen_Ray_hpp
+#define fensterchen_Ray_hpp
+#include <glm/glm.hpp> 
+
+struct Ray{
+    glm::vec3 origin;
+    glm:vec3 direcion;
+};
+#endif
