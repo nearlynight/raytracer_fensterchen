bd5d1f8749b9386a46b4af9bd272449cae99fec5
tfast <till@eduroam-053-024.scc.uni-weimar.de>
2015-03-24 17:55:16 +0100

    added default constructor

--- framework/Ray.hpp
+++ framework/Ray.hpp
@@ -1,7 +1,11 @@
 #include <glm/glm.hpp>
 
 struct Ray {
-	//Ray(glm::vec3 orig, glm::vec3 direc) : origin{orig}, direction{direc} {}
+	Ray() {
+		origin = glm::vec3();
+		direction = glm::vec3();
+	}
+	Ray(glm::vec3 orig, glm::vec3 direc) : origin{orig}, direction{direc} {}
 	glm::vec3 origin;
 	glm::vec3 direction;
 };
